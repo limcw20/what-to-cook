5710ef0e49eed250524d22da75f7fdf6855b3c3a
Edmund Lim <elimcw20@gmail.com>
2024-03-13 17:49:52 +0800

    Update fetch-test

--- codes/src/Components/Test.jsx
+++ codes/src/Components/Test.jsx
@@ -1,49 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React from "react";
 
-const MainRecipe = () => {
-  const [random, setRandom] = useState(null);
-
-  const getRandomRecipe = async () => {
-    try {
-      const res = await fetch("https://api.spoonacular.com/recipes/random", {
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key": "ca166215ff724312b0e1f673d7501f25",
-        },
-      });
-
-      if (res.ok) {
-        const data = await res.json();
-        setRandom(data);
-      } else {
-        throw new Error("Failed to fetch recipe");
-      }
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
-
-  // useEffect(() => {
-  //   getRandomRecipe();
-  // }, []);
-
-  return (
-    <div>
-      <Link to="/random">Go to Random Recipe</Link>
-
-      <div className="row">
-        <button className="col-md-3" onClick={getRandomRecipe}>
-          Get Random Recipe
-        </button>
-      </div>
-      {random && (
-        <div>
-          <p>{JSON.stringify(random)}</p>
-        </div>
-      )}
-    </div>
-  );
+const test = () => {
+  return <div></div>;
 };
 
-export default MainRecipe;
+export default test;
--- codes/src/Pages/Random.jsx
+++ codes/src/Pages/Random.jsx
@@ -1,49 +1,60 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import jsonData from "../Components/data.json";
 
-const MainRecipe = () => {
-  const [random, setRandom] = useState(null);
+const Random = () => {
+  const [allRandomRecipeData, setAllRandomRecipeData] = useState(jsonData);
+  const [filteredRandomRecipeData, setFilteredRandomRecipeData] =
+    useState(false);
 
-  const getRandomRecipe = async () => {
-    try {
-      const res = await fetch("https://api.spoonacular.com/recipes/random", {
-        headers: {
-          "Content-Type": "application/json",
-          "x-api-key": "ca166215ff724312b0e1f673d7501f25",
-        },
-      });
+  const handleClick = () => {
+    setFilteredRandomRecipeData(true);
+    // getRandomRecipeData();
+  };
 
-      if (res.ok) {
-        const data = await res.json();
-        setRandom(data);
-      } else {
-        throw new Error("Failed to fetch recipe");
-      }
-    } catch (error) {
-      console.error(error.message);
+  const getRandomRecipeData = async () => {
+    // try {
+    //   const res = await fetch("https://api.spoonacular.com/recipes/random", {
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       "x-api-key": "ca166215ff724312b0e1f673d7501f25",
+    //     },
+    //   });
+    if (res.ok) {
+      const data = await res.json();
+      setAllRandomRecipeData(data);
+    } else {
+      throw new Error("Failed to fetch recipe");
     }
+    // } catch (error) {
+    //   console.error(error.message);
+    // }
   };
 
   // useEffect(() => {
-  //   getRandomRecipe();
+  //   getRandomRecipeData();
   // }, []);
 
   return (
     <div>
-      {/* <Link to="/random">Go to Random Recipe</Link> */}
-
       <div className="row">
-        <button className="col-md-3" onClick={getRandomRecipe}>
+        <button className="col-md-3" onClick={handleClick}>
           Get Random Recipe
         </button>
       </div>
-      {random && (
+      {filteredRandomRecipeData && (
         <div>
-          <p>{JSON.stringify(random)}</p>
+          {/* <p>{JSON.stringify(allRandomRecipeData.recipes[0])}</p> */}
+          {allRandomRecipeData.recipes[0]?.image && (
+            <img src={allRandomRecipeData.recipes[0].image} />
+          )}
+
+          <p>{allRandomRecipeData.recipes[0].id}</p>
+          <p>{allRandomRecipeData.recipes[0].title}</p>
         </div>
       )}
     </div>
   );
 };
 
-export default MainRecipe;
+export default Random;
