2f545c1584edd1769f3f431b7c2d2a7b48ac4f6a
Edmund Lim <elimcw20@gmail.com>
2024-03-14 11:13:39 +0800

    Update RandomList-Commentout
    Commented out to prevent daily quota of API requests

--- codes/src/Pages/RandomList.jsx
+++ codes/src/Pages/RandomList.jsx
@@ -4,63 +4,34 @@ import jsonData from "../Components/data.json";
 // import SaveButton from "../Components/SaveButton";
 
 const RandomList = () => {
-  const [allRandomRecipeData, setAllRandomRecipeData] = useState([]);
-  // const [filteredRandomRecipeData, setFilteredRandomRecipeData] =
-  //   useState(false);
-
-  // const handleClick = () => {
-  //   setFilteredRandomRecipeData(true);
-  //   getRandomRecipeData();
-  // };
+  const [allRandomRecipeData, setAllRandomRecipeData] = useState(jsonData);
 
   const getRandomRecipeData = async () => {
-    try {
-      const res = await fetch(
-        "https://api.spoonacular.com/recipes/random?number=2",
-        {
-          headers: {
-            "Content-Type": "application/json",
-            "x-api-key": "ca166215ff724312b0e1f673d7501f25",
-          },
-        }
-      );
-      if (res.ok) {
-        const data = await res.json();
-        setAllRandomRecipeData(data);
-      } else {
-        throw new Error("Failed to fetch recipe");
-      }
-    } catch (error) {
-      console.error(error.message);
-    }
+    // try {
+    //   const res = await fetch(
+    //     "https://api.spoonacular.com/recipes/random?number=2",
+    //     {
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //         "x-api-key": "ca166215ff724312b0e1f673d7501f25",
+    //       },
+    //     }
+    //   );
+    //   if (res.ok) {
+    //     const data = await res.json();
+    //     setAllRandomRecipeData(data);
+    //   } else {
+    //     throw new Error("Failed to fetch recipe");
+    //   }
+    // } catch (error) {
+    //   console.error(error.message);
+    // }
   };
 
   useEffect(() => {
     getRandomRecipeData();
   }, []);
 
-  //   return (
-  //     <div>
-  //       <div className="row">
-  //         <button className="col-md-3" onClick={handleClick}>
-  //           Get Random Recipe
-  //         </button>
-  //       </div>
-  //       {filteredRandomRecipeData && (
-  //         <div>
-  //           {/* <p>{JSON.stringify(allRandomRecipeData.recipes[0])}</p> */}
-  //           {allRandomRecipeData.recipes[0]?.image && (
-  //             <img src={allRandomRecipeData.recipes[0].image} />
-  //           )}
-
-  //           <p>{allRandomRecipeData.recipes[0].id}</p>
-  //           <p>{allRandomRecipeData.recipes[0].title}</p>
-  //         </div>
-  //       )}
-  //       {/* <SaveButton></SaveButton> */}
-  //     </div>
-  //   );
-  // };
   return (
     <div>
       <div className="row">
